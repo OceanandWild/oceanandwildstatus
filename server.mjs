b1366297a52ff7cf7b66aab773c27669f65219b4
OceanandWild <149962867+OceanandWild@users.noreply.github.com>
2025-01-29 15:06:09 -0300

    Add files via upload

--- server.mjs
+++ server.mjs
@@ -32,5 +32,5 @@ app.post("/send-to-slack", async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
